21650ca29aeb0a5b39f8d3801810c3a561f2ea73
Matt Tognetti <mjtognetti@gmail.com>
2019-05-08 14:37:19 -0700

    Add main js

---
+++ dist/sundae-universal-landing-page/f/sundae-universal-landing-f-main.js
@@ -0,0 +1,67 @@
+const componentForm = {
+    street_number: 'short_name',
+    route: 'long_name',
+    locality: 'long_name',
+    administrative_area_level_1: 'long_name',
+    country: 'long_name',
+    postal_code: 'short_name'
+   };
+   
+   lp.jQuery(document).ready(function($) {
+    // Generate a GUID that we'll use to identify this lead in Salesforce for step 2 (contact info).
+    // We'll use a Zap to locate the newly created lead in order to update it with the contact info in
+    // the second page which is a different form altogether.
+    // Salesforce also requires that we set a lastname value, so we'll use it as a placeholder too
+    const guid = uuidv4();
+    const guidParts = guid.split('-');
+    $('#unbounce_guid').val(guid);
+    $('#last_name').val(`placeholder-${guidParts[0]}-${guidParts[1]}`);
+    $('#first_name').val(`placeholder-${guidParts[1]}-${guidParts[2]}`);
+    $('#email').val(`placeholder-${guidParts[1]}@${guidParts[0]}.com`);
+   
+   
+    // Setup autocomplete for any forms on the page
+    $('form').each((i, form) => {
+      const addressField = form.autocomplete_address;
+      const autocomplete = new google.maps.places.Autocomplete(addressField, {
+        types: ['geocode'],
+      });
+   
+      // get address components
+      const addresses = {};
+   
+      autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace();
+   
+        place.address_components.forEach((component) => {
+          const type = component.types[0];
+          addresses[type] = component[componentForm[type]] || "";
+        });
+   
+        form.address.value = (addresses.street_number || "") + " " + (addresses.route || "");
+        form.city.value = addresses.locality || "";
+        form.state.value = addresses.administrative_area_level_1 || "";
+        form.zip_code.value = addresses.postal_code || "";
+   
+        // Record whether the zip is in region
+        const isInRegion = zipsInRegion.indexOf(parseInt(form.zip_code.value)) > -1;
+        $('#in_region').val(isInRegion);
+      });
+    });
+   });
+   
+   
+   // smooth scrolling
+   lp.jQuery(function($) {
+    // The speed of the scroll in milliseconds
+    var speed = 500;
+   
+    // Find links that are #anchors and scroll to them
+    $('a[href^=#]')
+      .not('.lp-pom-form .lp-pom-button')
+      .unbind('click.smoothScroll')
+      .bind('click.smoothScroll', function(event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: $( $(this).attr('href') ).offset().top }, speed);
+      });
+   });
