ab1ac464434e1fa51b4004c3d7d864cece464715
Matt Tognetti <mjtognetti@gmail.com>
2019-05-08 17:57:11 -0700

    Add all page and variant scripts

---
+++ dist/autocomplete-address-step2-contact-info/a/autocomplete-address-step2-contact-info-a-fullstory.js
@@ -0,0 +1,26 @@
+  (function() {
+    // Tell FullStory the user's name once they enter something
+    function bindSetFullStoryVars() {
+        $('input[name*="_name"]').live('change', function() {
+          var firstName = $('#first_name').val();
+          var lastName  = $('#last_name').val();
+          if (typeof FS != "undefined") {
+            FS.setUserVars({
+              displayName: "" + firstName + " " + lastName
+             });
+          }
+        });
+      
+        // Send page variant to Fullstory
+        if (typeof FS != "undefined") {
+          FS.setUserVars({
+            pageVariant_str:window.ub.page.variantId
+          });
+        }
+      }
+      
+      lp.jQuery(document).ready(function($) {
+        bindSetFullStoryVars();
+      });
+    
+    })()
