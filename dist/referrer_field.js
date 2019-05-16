51210e9e3eff2553b4766369655c61c15e0a0e15
Julian Frumar <julian@frumar.com>
2019-05-16 15:13:00 -0700

    Allow unbounce to set the referrer field via a URL param if set.

--- dist/referrer_field.js
+++ dist/referrer_field.js
@@ -1,6 +1,26 @@
-// If a referrer field exists on the page, populate it with the document.referrer value
-lp.jQuery(function($) {
-    $('input[name="referrer"]').each(function(i, field) {
-        $(field).val(document.referrer);
+(function(){
+    // If a referrer field exists on the page, populate it with the document.referrer value
+    lp.jQuery(function($) {
+        // Bail out if the referrer was passed through a URL param.
+        // This can happen when the SFDC lead is not generated upfront.
+        if (getUrlParameter('referrer')) return;
+        $('input[name="referrer"]').each(function(i, field) {
+            $(field).val(document.referrer);
+        });
     });
-});
+
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    };
+})();
