da52bef771077c144dc31cc47e4fa953355bbec0
Matt Tognetti <mjtognetti@gmail.com>
2019-05-22 12:45:15 -0700

    Add activity and experience pages

---
+++ dist/investor-experience/a/main.js
@@ -0,0 +1,53 @@
+(function() {
+  // Load the HTML
+  var formHtmlUrl =
+    "http://localhost:8080/dist/investor-experience/a/form.html";
+  var containerId = "#dynamic-root";
+
+  lp.jQuery(document).ready(function($) {
+    var $container = $(containerId);
+    $.get(formHtmlUrl, function(data) {
+      $container.html(data);
+      initForm();
+
+      // fix positioning
+      $container.parent().css({
+        position: "relative",
+        overflow: "auto",
+        height: "auto"
+      });
+    });
+  });
+
+  // Init the form
+  function initForm() {
+    var $unbounceForm = $("lp-pom-form form");
+    var $dynamicForm = $("#dynamic-root form");
+    var $backButton = $dynamicForm.find(".BackButton");
+
+    $backButton.click(goBack);
+    $dynamicForm.submit(onSubmit);
+
+    // Submit the unbounce form instead when the visible form is submitted.
+    function onSubmit(event) {
+      event.preventDefault();
+      syncValues();
+      $unbounceForm.submit();
+    }
+
+    // Sync values between the visible form and the hidden unbounce form
+    // so we can submit the entire unbounce form.
+    function syncValues() {
+      var $dynamicFields = $dynamicForm.find("input");
+      $dynamicFields.each(function(index, input) {
+        var $input = $(input);
+        var $unbounceInput = $("#unbounce-" + $input.attr("name"));
+        $unbounceInput.val($input.val());
+      });
+    }
+
+    function goBack() {
+      window.history.back();
+    }
+  }
+})();
