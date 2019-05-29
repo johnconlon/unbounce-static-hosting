1f9e451fe6ea6a95001bc9b0a4885082a31bfb71
Matt Tognetti <mjtognetti@gmail.com>
2019-05-28 22:09:42 -0700

    Fix owner value and guid syntax for IE10

--- dist/unbounce_guid.js
+++ dist/unbounce_guid.js
@@ -2,8 +2,7 @@
   // Generate a UUID that we'll use to identify this lead in Salesforce. Useful in
   // multi-step flows for updating existing leads.
   lp.jQuery(document).ready(function($) {
-    const guid = uuidv4();
-    const guidParts = guid.split("-");
+    var guid = uuidv4();
     $("#unbounce_guid").val(guid);
-  })
-})()
+  });
+})();
