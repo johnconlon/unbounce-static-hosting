e23e0402467c4a09a0385e5a869007e7d82e2a35
Matt Tognetti <mjtognetti@gmail.com>
2019-06-03 12:25:35 -0700

    Add multiple entry configs and dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
 const path = require("path");
+const fs = require("fs");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require("dotenv-webpack")
+const glob = require("glob")
 
-module.exports = {
-  entry: path.resolve(__dirname, "src/pages/investor-landing/a"),
-  output: {
-    path: path.resolve(__dirname, "dist/pages/investor-landing/a"),
-    filename: "[name].js"
-  },
+// A dash-separated name for an entry. For example: src/pages/investor/a would be
+// pages-investor-a.
+const entryName = entry => path.dirname(relativeEntry(entry)).split("/").join("-")
+
+// The relative path of an entry, e.g. pages/investor/a
+const relativeEntry = entry => path.relative(path.resolve(__dirname, "src"), entry)
+
+// The absolute dist/output path of an entry
+const distPath = entry => path.dirname(path.resolve(__dirname, "dist", relativeEntry(entry)))
+
+// Configuration options shared across all entries.
+const shared = {
   plugins: [
     new MiniCssExtractPlugin({
       filename: "[name].css"
+    }),
+    new Dotenv({
+      path: './.env.development'
     })
   ],
   resolve: {
@@ -33,3 +45,22 @@ module.exports = {
     ]
   }
 };
+
+const config = entry =>
+  Object.assign(
+    {},
+    shared,
+    {
+      name: entryName(entry),
+      entry: entry,
+      output: {
+        path: distPath(entry),
+        filename: "[name].js"
+      },
+    }
+  )
+
+// Find all entry points and map them to webpack configs.
+const entries = glob.sync("./src/**/*/index.@(ts|js)", { ignore: "**/node_modules/**" })
+module.exports = entries.map(config)
+
