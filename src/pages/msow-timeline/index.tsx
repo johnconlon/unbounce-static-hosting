e4b4790d2f253625f4a781b5e40df78c3205f9a1
Matt Tognetti <mjtognetti@gmail.com>
2019-07-03 16:00:47 -0700

    Track conversion on the timeline page

--- src/pages/msow-timeline/index.tsx
+++ src/pages/msow-timeline/index.tsx
@@ -1,6 +1,8 @@
 import "promise-polyfill/dist/polyfill";
 import * as Unbounce from "unbounce";
 import { render, h } from "preact";
+import * as facebook from "facebook"
+import { ready, getUrlParameter } from "dom-utils"
 
 import UnbounceQuestionContainer from "../../components/UnbounceQuestionContainer"
 import TimelineQuestion from "../../components/msow/TimelineQuestion";
@@ -8,6 +10,19 @@ import TimelineQuestion from "../../components/msow/TimelineQuestion";
 import "normalize.css";
 import "sundaelib/css/reset.css";
 import "sundaelib/css/theme.css";
+
+// TODO: formalize this
+// Track a conversion if this lead is in region. Detect in region by
+// looking at the url parameters.
+ready(function() {
+  const inRegion = getUrlParameter("in_region")
+  if (!inRegion) {
+    console.warn("Missing the in_region url parameter")
+  } else if (inRegion === "true") {
+      facebook.trackConversion()
+  }
+})
+
 
 Unbounce.init(
   "dynamic-root",
