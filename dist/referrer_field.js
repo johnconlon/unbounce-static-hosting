8e6e19bbae432813355e4f86450e1d6c58668b5e
Matt Tognetti <mjtognetti@gmail.com>
2019-07-03 11:35:49 -0700

    Remove jquery from referrer_field.js

--- dist/referrer_field.js
+++ dist/referrer_field.js
@@ -1,26 +1,41 @@
-(function(){
-    // If a referrer field exists on the page, populate it with the document.referrer value
-    lp.jQuery(function($) {
-        // Bail out if the referrer was passed through a URL param.
-        // This can happen when the SFDC lead is not generated upfront.
-        if (getUrlParameter('referrer')) return;
-        $('input[name="referrer"]').each(function(i, field) {
-            $(field).val(document.referrer);
-        });
+(function() {
+  // If a referrer field exists on the page, populate it with the document.referrer value
+  ready(function() {
+    // Bail out if the referrer was passed through a URL param.
+    // This can happen when the SFDC lead is not generated upfront.
+    if (getUrlParameter("referrer")) return;
+    var fields = document.querySelectorAll('input[name="referrer"]');
+    Array.prototype.forEach.call(fields, function(field) {
+      field.value = document.referrer;
     });
+  });
 
-    function getUrlParameter(sParam) {
-        var sPageURL = window.location.search.substring(1),
-            sURLVariables = sPageURL.split('&'),
-            sParameterName,
-            i;
+  function ready(fn) {
+    if (
+      document.attachEvent
+        ? document.readyState === "complete"
+        : document.readyState !== "loading"
+    ) {
+      fn();
+    } else {
+      document.addEventListener("DOMContentLoaded", fn);
+    }
+  }
 
-        for (i = 0; i < sURLVariables.length; i++) {
-            sParameterName = sURLVariables[i].split('=');
+  function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+      sURLVariables = sPageURL.split("&"),
+      sParameterName,
+      i;
 
-            if (sParameterName[0] === sParam) {
-                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-            }
-        }
-    };
+    for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split("=");
+
+      if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined
+          ? true
+          : decodeURIComponent(sParameterName[1]);
+      }
+    }
+  }
 })();
