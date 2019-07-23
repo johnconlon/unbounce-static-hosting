2f3823b61cb7cf70ad8776baade5c35a93fbeb2c
Nam Kim <nam@sundae.com>
2019-07-23 03:46:32 +0000

    add scripts for q variant

---
+++ dist/sundae-universal-landing-page/q/sundae-universal-landing-q-fullstory.js
@@ -0,0 +1,15 @@
+(function() {
+  // Tell FullStory the user's name once they enter something
+  function bindSetFullStoryVars() {
+    // Send page variant to Fullstory
+    if (typeof FS != "undefined") {
+      FS.setUserVars({
+        pageVariant_str: window.ub.page.variantId
+      });
+    }
+  }
+
+  $(document).ready(function($) {
+    bindSetFullStoryVars();
+  });
+})();
