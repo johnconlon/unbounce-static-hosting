b3ae53043a87cffee8dfb6d067c0f83c6d13a3ec
Nam Kim <nam@sundae.com>
2019-07-23 06:21:13 +0000

    recopy from f variant

--- dist/sundae-universal-landing-page/q/sundae-universal-landing-q-fullstory.js
+++ dist/sundae-universal-landing-page/q/sundae-universal-landing-q-fullstory.js
@@ -1,14 +1,24 @@
 (function() {
   // Tell FullStory the user's name once they enter something
   function bindSetFullStoryVars() {
+    $('input[name*="_name"]').live('change', function() {
+      var firstName = $('#first_name').val();
+      var lastName  = $('#last_name').val();
+      if (typeof FS != "undefined") {
+        FS.setUserVars({
+          displayName: "" + firstName + " " + lastName
+         });
+      }
+    });
+  
     // Send page variant to Fullstory
     if (typeof FS != "undefined") {
       FS.setUserVars({
-        pageVariant_str: window.ub.page.variantId
+        pageVariant_str:window.ub.page.variantId
       });
     }
   }
-
+    
   $(document).ready(function($) {
     bindSetFullStoryVars();
   });
--- dist/sundae-universal-landing-page/q/sundae-universal-landing-q-main.js
+++ dist/sundae-universal-landing-page/q/sundae-universal-landing-q-main.js
@@ -1,81 +1,70 @@
 (function() {
-  const componentForm = {
-    street_number: "short_name",
-    route: "long_name",
-    locality: "long_name",
-    administrative_area_level_1: "long_name",
-    country: "long_name",
-    postal_code: "short_name"
-  };
-
-  $(document).ready(function($) {
+const componentForm = {
+    street_number: 'short_name',
+    route: 'long_name',
+    locality: 'long_name',
+    administrative_area_level_1: 'long_name',
+    country: 'long_name',
+    postal_code: 'short_name'
+};
+   
+jQuery(document).ready(function($) {
     // Generate a GUID that we'll use to identify this lead in Salesforce for step 2 (contact info).
     // We'll use a Zap to locate the newly created lead in order to update it with the contact info in
     // the second page which is a different form altogether.
     // Salesforce also requires that we set a lastname value, so we'll use it as a placeholder too
     const guid = uuidv4();
-    const guidParts = guid.split("-");
-    $("#unbounce_guid").val(guid);
-
+    const guidParts = guid.split('-');
+    $('#unbounce_guid').val(guid);
+   
     // Setup autocomplete for any forms on the page
-    $("form").each(function(i, form) {
+    $('form').each(function(i, form) {
       const addressField = form.autocomplete_address;
       const autocomplete = new google.maps.places.Autocomplete(addressField, {
-        types: ["geocode"]
+        types: ['geocode'],
       });
-
+   
       // get address components
       const addresses = {};
-
-      autocomplete.addListener("place_changed", function() {
+   
+      autocomplete.addListener('place_changed', function() {
         const place = autocomplete.getPlace();
-        const geometry = place.geometry;
 
         if (!Array.isArray(place.address_components)) {
-          console.warn(
-            "place.address_components is not an Array. Will not process autosuggested address."
-          );
+          console.warn('place.address_components is not an Array. Will not process autosuggested address.');
           return;
         }
-
+    
         place.address_components.forEach(function(component) {
           const type = component.types[0];
           addresses[type] = component[componentForm[type]] || "";
         });
-
-        form.address.value =
-          (addresses.street_number || "") + " " + (addresses.route || "");
+   
+        form.address.value = (addresses.street_number || "") + " " + (addresses.route || "");
         form.city.value = addresses.locality || "";
         form.state.value = addresses.administrative_area_level_1 || "";
         form.zip_code.value = addresses.postal_code || "";
-        form.lat.value = geometry ? geometry.location.lat() : "";
-        form.lng.value = geometry ? geometry.location.lng() : "";
-
+   
         // Record whether the zip is in region
-        const isInRegion =
-          zipsInRegion.indexOf(parseInt(form.zip_code.value)) > -1;
-        $("#in_region").val(isInRegion);
-
-        $(form).submit();
+        const isInRegion = zipsInRegion.indexOf(parseInt(form.zip_code.value)) > -1;
+        $('#in_region').val(isInRegion);
       });
     });
-  });
-
-  // smooth scrolling
-  $(function($) {
+   });
+   
+   
+   // smooth scrolling
+   $(function($) {
     // The speed of the scroll in milliseconds
     var speed = 400;
-
+   
     // Find links that are #anchors and scroll to them
-    $("a[href^=\\#]")
-      .not(".lp-pom-form .lp-pom-button")
-      .unbind("click.smoothScroll")
-      .bind("click.smoothScroll", function(event) {
+    $('a[href^=\\#]')
+      .not('.lp-pom-form .lp-pom-button')
+      .unbind('click.smoothScroll')
+      .bind('click.smoothScroll', function(event) {
         event.preventDefault();
-        $("html, body").animate(
-          { scrollTop: $($(this).attr("href")).offset().top },
-          speed
-        );
+        $('html, body').animate({ scrollTop: $( $(this).attr('href') ).offset().top }, speed);
       });
-  });
-})();
+   });
+})()
