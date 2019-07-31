95c093367b09b50fef78f16fa07d8a8aff6ef98e
Nam Kim <nam@sundae.com>
2019-07-31 23:29:53 +0000

    scripts for d variant

---
+++ dist/autocomplete-address-step2-contact-info/d/autocomplete-address-step2-contact-info-a.js
@@ -0,0 +1,48 @@
+(function() {
+const QUALIFIED_LEAD_THANK_YOU_PAGE = 'https://sundae.com/thank-you/';
+const NON_QUALIFIED_LEAD_THANK_YOU_PAGE = 'https://sundae.com/thank-you-nql/';
+
+// Direct the user to a different page depending on whether their zip is in region or not
+// We'll use the thank-you page with the FB "Lead" tracking event if the zip is in region
+// otherwise they go to a different thank-you page without the lead tracking event.
+function bindFormRedirectOnSubmit() {
+  $('input').live('change', function () {
+    const enteredZip = $('form').get(0).zip_code.value.trim();
+    const isInRegion = zipsInRegion.indexOf(parseInt(enteredZip)) > -1;
+    if (!isInRegion) {
+      window.module.lp.form.data.url = QUALIFIED_LEAD_THANK_YOU_PAGE;
+    } else {
+      // Use the configured MSOW flow
+    }
+    $('.lp-pom-form [name="in_region"]').val(isInRegion);
+  });
+}
+
+// Ensure the referrer field is fewer than 255 characters to not break a backend
+// integration.
+function trimReferrer(args) {
+  var referrerInput = args.formElement.querySelector("#referrer");
+  var withoutQueryParams = referrerInput.value.split("?")[0];
+  referrerInput.value = withoutQueryParams;
+}
+
+// smooth scrolling
+lp.jQuery(function ($) {
+  // The speed of the scroll in milliseconds
+  var speed = 400;
+
+  // Find links that are #anchors and scroll to them
+  $('a[href^=#]')
+    .not('.lp-pom-form .lp-pom-button')
+    .unbind('click.smoothScroll')
+    .bind('click.smoothScroll', function (event) {
+      event.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, speed);
+    });
+});
+
+lp.jQuery(document).ready(function ($) {
+  bindFormRedirectOnSubmit();
+  window.ub.hooks.beforeFormSubmit.push(trimReferrer)
+});
+})()
