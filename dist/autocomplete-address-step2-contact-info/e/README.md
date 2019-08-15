4cb1344bebe56f2baec157817f67778f4e2e192b
Nam Kim <nam@sundae.com>
2019-08-15 20:57:12 +0000

    contact form e variant for msow flow

---
+++ dist/autocomplete-address-step2-contact-info/e/README.md
@@ -0,0 +1,9 @@
+## Variant E
+
+Simple 4-field form with:
+* First name
+* Last name
+* Email
+* Phone
+
+*Redirects to MSOW flow.
---
+++ dist/autocomplete-address-step2-contact-info/e/autocomplete-address-step2-contact-info-e.js
@@ -0,0 +1,28 @@
+(function() {
+// Ensure the referrer field is fewer than 255 characters to not break a backend
+// integration.
+function trimReferrer(args) {
+  var referrerInput = args.formElement.querySelector("#referrer");
+  var withoutQueryParams = referrerInput.value.split("?")[0];
+  referrerInput.value = withoutQueryParams;
+}
+
+// smooth scrolling
+lp.jQuery(function ($) {
+  // The speed of the scroll in milliseconds
+  var speed = 400;
+
+  // Find links that are #anchors and scroll to them
+  $('a[href^=#]')
+    .not('.lp-pom-form .lp-pom-button')
+    .unbind('click.smoothScroll')
+    .bind('click.smoothScroll', function (event) {
+      event.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, speed);
+    });
+});
+
+lp.jQuery(document).ready(function ($) {
+  window.ub.hooks.beforeFormSubmit.push(trimReferrer)
+});
+})()
