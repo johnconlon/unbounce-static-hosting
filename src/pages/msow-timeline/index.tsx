0efc59c4d859bd330a3b72542e1ab717942d0bd2
Nam Kim <nam@sundae.com>
2019-08-22 05:49:22 +0000

    remove facebook conversion code; moving to unbounce since that's where scripts are fetched

--- src/pages/msow-timeline/index.tsx
+++ src/pages/msow-timeline/index.tsx
@@ -1,7 +1,6 @@
 import "promise-polyfill/dist/polyfill";
 import * as Unbounce from "unbounce";
 import { render, h } from "preact";
-import * as facebook from "facebook"
 import { ready, getUrlParameter } from "dom-utils"
 
 import UnbounceQuestionContainer from "../../components/UnbounceQuestionContainer"
@@ -12,19 +11,6 @@ import QuestionForm from "../../components/QuestionForm";
 import "normalize.css";
 import "sundaelib/css/reset.css";
 import "sundaelib/css/theme.css";
-
-// TODO: formalize this
-// Track a conversion if this lead is in region. Detect in region by
-// looking at the url parameters.
-ready(function() {
-  const inRegion = getUrlParameter("in_region")
-  if (!inRegion) {
-    console.warn("Facebook Conversion Tracking: Missing the in_region url parameter, won't track as a conversion")
-  } else if (inRegion === "true") {
-      facebook.trackConversion()
-  }
-})
-
 
 if (Unbounce.isUnbounce()) {
 Unbounce.init(
